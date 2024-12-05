5cc44119096b5c476bb41391a5493d1af68c0922
Miguel Trepa <miguel.t.m.goncalves@tecnico.ulisboa.pt>
2024-12-05 17:54:46 +0000

    h

--- proj.cpp
+++ proj.cpp
@@ -18,6 +18,14 @@ void setSequence(int m, vector<int> *sequence) {
     }
 }
 
+vector<int> calculatePossibleResults(int j, int i, vector<vector<int>> *operations, vector<int> *sequence, vector<vector<vector<int>>> *results) {
+	vector<int> possibleResults;
+
+	
+
+	return possibleResults;
+}
+
 int main(int argc, char *argv[]) {
     std::ios::sync_with_stdio(0);
     std::cin.tie(0);
@@ -29,12 +37,12 @@ int main(int argc, char *argv[]) {
     cin >> n >> m;
 
     /* iniciar a matrix com os resultados das operações*/
-    vector<vector<int>> operations(n, vector<int>(n));      // matrix of size n x n with operations
-    setOperations(n, &operations);
+    vector<vector<int>> operacoes(n, vector<int>(n));      // matrix of size n x n with operations
+    setOperations(n, &operacoes);
 
     /* iniciar o vetor com a sequência a resolver */
-    vector<int> sequence(m);
-    setSequence(m, &sequence);
+    vector<int> sequencia(m);
+    setSequence(m, &sequencia);
 
     /* valor a encontrar */
     cin >> r;
@@ -42,11 +50,21 @@ int main(int argc, char *argv[]) {
     /* matrix para resolver o problema */
     vector<vector<vector<int>>> matrizDeResultados(m, vector<vector<int>>(m));  // m x m x k, em que k são as posições dos parenteses
     
+	/* inserir os valores iniciais na matriz (a diagonal central) */
     for (int i = 0; i < m; i++) {
-        matrizDeResultados[i][i].push_back(sequence[i]);    // casos base
+        matrizDeResultados[i][i].push_back(sequencia[i]);
     }
 
-    for (int start = 0; )
+    for (int start = 0; start < m; start++) {
+        int j = start, i = 0;               // j = coluna, i = linha
+
+        while (j < m) {                     // isto vai iterar sobre a matriz diagonalmente
+            matrizDeResultados[j][i] = calculatePossibleResults(j, i, &operacoes, &sequencia, &matrizDeResultados);
+			j++;
+			i++;
+        }
+		cout << endl;
+    }
 
     return 0;
 }
