850409e47310458d957706948b6dc83b352ced86
Miguel Trepa <miguel.t.m.goncalves@tecnico.ulisboa.pt>
2024-12-05 20:01:53 +0000

    Co-authored-by: wjana298 <wjana298@users.noreply.github.com>

--- proj.cpp
+++ proj.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+
+// Função para ler a matriz de operações
 void setOperations(int n, vector<vector<int>> *operations) {
     for (int i = 0; i < n; ++i) {                           // reading the matrix
         for (int j = 0; j < n; ++j) {                       // quando o projeto estiver pronto, mudar para argv
@@ -12,21 +14,38 @@ void setOperations(int n, vector<vector<int>> *operations) {
     }
 }
 
+// Função para ler a sequência
 void setSequence(int m, vector<int> *sequence) {
     for (int i = 0; i < m; i++){                             // reading the sequence
         cin >> (*sequence)[i];
     }
 }
 
-vector<int> calculatePossibleResults(int j, int i, vector<vector<int>> *operations, vector<int> *sequence, vector<vector<vector<int>>> *results) {
-	vector<int> possibleResults;
+// Função para fazer operações entre listas de valores
+void operation(vector<int> a, vector<int> b, vector<vector<int>> *operations) {
+    for (size_t i = 0; i < a.size(); i++) {
+        for (size_t j = 0; j < b.size(); j++) {
+            cout << a[i] << " " << b[j] << " " << (*operations)[a[i]][b[j]] << endl;
+        }
+    }
+}
 
-	
+vector<pair<vector<int>, int>> calculatePossibleResults(int i, int j, vector<vector<int>> *operations, vector<int> *sequence, vector<vector<vector<pair<vector<int>, int>>>> *matrizDeResultados) {
+	vector<pair<vector<int>, int>> possibleResults;
+
+    if (i == j) {
+        possibleResults.push_back({{(*sequence)[i]}, -1});
+        return possibleResults;
+    }
+
+    for (int k = j -1; k >= i; k--) {
+        operation((*matrizDeResultados)[i][k], (*matrizDeResultados)[k+1][j], operations);
+    }
 
 	return possibleResults;
 }
 
-int main(int argc, char *argv[]) {
+int main() {
     std::ios::sync_with_stdio(0);
     std::cin.tie(0);
 
@@ -48,19 +67,15 @@ int main(int argc, char *argv[]) {
     cin >> r;
 
     /* matrix para resolver o problema */
-    vector<vector<vector<int>>> matrizDeResultados(m, vector<vector<int>>(m));  // m x m x k, em que k são as posições dos parenteses
-    
-	/* inserir os valores iniciais na matriz (a diagonal central) */
-    for (int i = 0; i < m; i++) {
-        matrizDeResultados[i][i].push_back(sequencia[i]);
-    }
+    vector<vector<vector<pair<vector<int>, int>>>> matrizDeResultados(m, vector<vector<pair<vector<int>, int>>>(m));  // m x m x k, em que k são as posições dos parenteses
 
     for (int start = 0; start < m; start++) {
-        int j = start, i = 0;               // j = coluna, i = linha
+        int i = 0, j = start;               // j = coluna, i = linha
 
-        while (j < m) {                     // isto vai iterar sobre a matriz diagonalmente
-            matrizDeResultados[j][i] = calculatePossibleResults(j, i, &operacoes, &sequencia, &matrizDeResultados);
-			j++;
+        while (i < m) {                     // isto vai iterar sobre a matriz diagonalmente
+			cout << i << " " << j << "\t";
+            matrizDeResultados[i][j] = calculatePossibleResults(i, j, &operacoes, &sequencia, &matrizDeResultados);
+            j++;
 			i++;
         }
 		cout << endl;
